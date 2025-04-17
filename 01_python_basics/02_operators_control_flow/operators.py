1c56eeaeb4fe6bb630fbcc48b20f58f6519df1bc
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-04-17 12:54:43 +0300

    python intro

---
+++ 01_python_basics/02_operators_control_flow/operators.py
@@ -0,0 +1,17 @@
+# Arithmetic
+"""
+    +, -, /, *, //, **
+"""
+# Comparison/ Relational
+# Logical
+# Assignment
+"""
+    +=, -=, /=, *=, //=, **=
+    
+    a = 12
+    a = a + 12
+    a += 12
+"""
+# Bitwise
+# Identity
+# Membership operator
